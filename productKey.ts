8d463352efe42e237fd4240af9d570a81d2daa25
Hanbyeol Jo <60456752+PinMIlk@users.noreply.github.com>
2020-07-25 21:14:13 +0900

    Create productKey.ts

---
+++ productKey.ts
@@ -0,0 +1,10 @@
+import * as Process from "child_process";
+
+Process.exec('reg query \"HKEY_LOCAL_MACHINE\\SOFTWARE\\Microsoft\\Windows NT\\CurrentVersion\\SoftwareProtectionPlatform\" /v BackupProductKeyDefault', (error, stdout, stderr) => {
+    console.log('Product key: ', stdout.match(/[\dA-Z]+-[\dA-Z]+-[\dA-Z]+-[\dA-Z]+-[\dA-Z]+/i)[0]);
+    console.log('Press any key to exit');
+});
+
+process.stdin.setRawMode(true);
+process.stdin.resume();
+process.stdin.on('data', process.exit.bind(process, 0));
