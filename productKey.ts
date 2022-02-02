7b03eee41173fd682374877c452cbbe526f1a9b1
PinMilk <hanbyeul04@kakao.com>
2022-02-03 02:55:53 +0900

    commit

--- productKey.ts
+++ productKey.ts
@@ -1,10 +1,15 @@
-import * as Process from "child_process";
+import * as Process from "child_process"
 
-Process.exec('reg query \"HKEY_LOCAL_MACHINE\\SOFTWARE\\Microsoft\\Windows NT\\CurrentVersion\\SoftwareProtectionPlatform\" /v BackupProductKeyDefault', (error, stdout, stderr) => {
-    console.log('Product key: ', stdout.match(/[\dA-Z]+-[\dA-Z]+-[\dA-Z]+-[\dA-Z]+-[\dA-Z]+/i)[0]);
-    console.log('Press any key to exit');
-});
+Process.exec(
+    'reg query \"HKEY_LOCAL_MACHINE\\SOFTWARE\\Microsoft\\Windows NT\\CurrentVersion\\SoftwareProtectionPlatform\" /v BackupProductKeyDefault',
+    (_error, stdout) => {
+        const regex = /[\dA-Z]+-[\dA-Z]+-[\dA-Z]+-[\dA-Z]+-[\dA-Z]+/gi
+        const productKey: string = (stdout.match(regex) || ['XXXXX-XXXXX-XXXXX-XXXXX-XXXXX'])[0]
+        console.log('Product key: ', productKey)
+        console.log('Press any key to exit')
+    }
+);
 
-process.stdin.setRawMode(true);
-process.stdin.resume();
-process.stdin.on('data', process.exit.bind(process, 0));
+process.stdin.setRawMode(true)
+process.stdin.resume()
+process.stdin.on('data', process.exit.bind(process, 0))
